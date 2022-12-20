1c6fc0113911ad52843bff0e9c5caac439a8cb42
ChiodaPietro <chioda.pietro@studenti.salesianisesto.it>
2022-12-20 09:58:34 +0100

    boh

---
+++ src/utility/Card.java
@@ -0,0 +1,10 @@
+package utility;
+
+public class Card {
+    private String number;
+    private String symbol;
+    public Card(String number, String symbol){
+        this.number=number;
+        this.symbol=symbol;
+    }
+}
